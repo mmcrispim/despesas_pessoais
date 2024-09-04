1c5be3afbe6f1d4d6413d2e72e9e7072fb73da46
Mariangela Medeiros Crispim Araujo <mmcrispim@yahoo.com.br>
2024-09-04 18:08:33 -0300

    feat: desenvolvendo rotina de leitura do arquivo e tratamento dos campos

--- despesas_pessoais/main.py
+++ despesas_pessoais/main.py
@@ -1,23 +1,62 @@
 import pandas as pd
 
+# Caminho para o arquivo CSV
+caminho_arquivo = './input/lanctos_receitas_despesas.csv'
 
-# Função para ler o CSV, realizar transformações e salvar o resultado
-def process_despesas(input_path, output_path):
-    # Lendo o arquivo CSV
-    df = pd.read_csv(input_path, delimiter=';', encoding='utf-8')
+print(f'Caminho {caminho_arquivo}')
 
-    # Exemplo de transformação: Selecionar apenas algumas colunas e renomeá-las
-    df_transformed = df[['DT COMPRA', 'DESPESA', 'VLR TOTAL', 'STATUS']].copy()
-    df_transformed.columns = ['Data', 'Descrição', 'Valor', 'Status']
+# Leitura do arquivo CSV
+df = pd.read_csv(caminho_arquivo, header=0, sep=';')
 
-    # Exemplo de transformação: Filtrar apenas as despesas pagas
-    df_transformed = df_transformed[df_transformed['Status'] == 'Pago']
+# Contar o número de registros (linhas)
+numero_registros = len(df)
 
-    # Salvando o DataFrame transformado em um novo arquivo CSV
-    df_transformed.to_csv(output_path, index=False, sep=';')
+# Verificar se o arquivo tem dados
+print()  # Linha em branco para separar a saída de cada coluna
+if numero_registros > 0:
+    print(f'O arquivo contém {numero_registros} registros.')
+else:
+    print('O arquivo está vazio.')
 
+# Exibindo as primeiras 5 linhas do DataFrame
+print()  # Linha em branco para separar a saída de cada coluna
+print(f'Amostra do arquivo {df.head()}')
 
-if __name__ == '__main__':
-    input_file = './input/lanctos_receitas_despesas.csv'
-    output_file = './output/lancamento.csv'
-    process_despesas(input_file, output_file)
+# Renomear colunas
+df.rename(
+    columns={
+        'DT COMPRA': 'DT_COMPRA',
+        'RECEITA DESPESA': 'RECEITA_DESPESA',
+        'FORMA PAGTO': 'FORMA_PAGTO',
+        'VLR TOTAL': 'VLR_TOTAL',
+        'QTD PARCELAS': 'QTD_PARCELAS',
+        'NUM PARCELA': 'NUM_PARCELA',
+    },
+    inplace=True,
+)
+
+# Exibindo as primeiras 5 linhas do DataFrame
+print()  # Linha em branco para separar a saída de cada coluna
+print(f'Amostra do arquivo com colunas renomeadas{df.head()}')
+
+# Lista de colunas para as quais você deseja obter valores distintos
+colunas = [
+    'RECEITA_DESPESA',
+    'FORMA_PAGTO',
+    'TIPO',
+    'CATEGORIA',
+    'ITEM',
+    'STATUS',
+]
+
+# Dicionário para armazenar os DataFrames com valores distintos de cada coluna
+valores_distintos = {
+    coluna: df[coluna].drop_duplicates() for coluna in colunas
+}
+
+# Exibir os valores distintos de cada coluna
+for coluna, valores in valores_distintos.items():
+    print(f"Valores distintos na coluna '{coluna}':")
+    print(valores)
+    print()  # Linha em branco para separar a saída de cada coluna
+
