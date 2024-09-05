33c6b2e5f2711e02bbd7768f77bc106310b79286
Mariangela Medeiros Crispim Araujo <mmcrispim@yahoo.com.br>
2024-09-05 18:06:30 -0300

    feat: Ajuste da rotina incluindo criação de coluna VLR_PARCELA e novo tratamento dos dataframes dimensões

--- despesas_pessoais/main.py
+++ despesas_pessoais/main.py
@@ -1,62 +1,98 @@
+# main.py
 import pandas as pd
 
-# Caminho para o arquivo CSV
-caminho_arquivo = './input/lanctos_receitas_despesas.csv'
 
-print(f'Caminho {caminho_arquivo}')
+# Função principal
+def processar_csv(caminho_arquivo_entrada, caminho_arquivo_saida):
 
-# Leitura do arquivo CSV
-df = pd.read_csv(caminho_arquivo, header=0, sep=';')
+    print(f'Caminho {caminho_arquivo_entrada}')
 
-# Contar o número de registros (linhas)
-numero_registros = len(df)
+    # Leitura do arquivo CSV
+    df = pd.read_csv(caminho_arquivo_entrada, header=0, sep=';', decimal=',')
 
-# Verificar se o arquivo tem dados
-print()  # Linha em branco para separar a saída de cada coluna
-if numero_registros > 0:
-    print(f'O arquivo contém {numero_registros} registros.')
-else:
-    print('O arquivo está vazio.')
+    # Contar o número de registros (linhas)
+    numero_registros = len(df)
 
-# Exibindo as primeiras 5 linhas do DataFrame
-print()  # Linha em branco para separar a saída de cada coluna
-print(f'Amostra do arquivo {df.head()}')
-
-# Renomear colunas
-df.rename(
-    columns={
-        'DT COMPRA': 'DT_COMPRA',
-        'RECEITA DESPESA': 'RECEITA_DESPESA',
-        'FORMA PAGTO': 'FORMA_PAGTO',
-        'VLR TOTAL': 'VLR_TOTAL',
-        'QTD PARCELAS': 'QTD_PARCELAS',
-        'NUM PARCELA': 'NUM_PARCELA',
-    },
-    inplace=True,
-)
-
-# Exibindo as primeiras 5 linhas do DataFrame
-print()  # Linha em branco para separar a saída de cada coluna
-print(f'Amostra do arquivo com colunas renomeadas{df.head()}')
-
-# Lista de colunas para as quais você deseja obter valores distintos
-colunas = [
-    'RECEITA_DESPESA',
-    'FORMA_PAGTO',
-    'TIPO',
-    'CATEGORIA',
-    'ITEM',
-    'STATUS',
-]
-
-# Dicionário para armazenar os DataFrames com valores distintos de cada coluna
-valores_distintos = {
-    coluna: df[coluna].drop_duplicates() for coluna in colunas
-}
-
-# Exibir os valores distintos de cada coluna
-for coluna, valores in valores_distintos.items():
-    print(f"Valores distintos na coluna '{coluna}':")
-    print(valores)
+    # Verificar se o arquivo tem dados
     print()  # Linha em branco para separar a saída de cada coluna
+    if numero_registros > 0:
+        print(f'O arquivo contém {numero_registros} registros.')
+    else:
+        print('O arquivo está vazio.')
 
+    # Exibindo as primeiras 5 linhas do DataFrame
+    print()  # Linha em branco para separar a saída de cada coluna
+    print(f'Amostra do arquivo {df.head()}')
+
+    # Renomear colunas
+    df.rename(
+        columns={
+            'DT COMPRA': 'DT_COMPRA',
+            'RECEITA DESPESA': 'RECEITA_DESPESA',
+            'FORMA PAGTO': 'FORMA_PAGTO',
+            'VLR TOTAL': 'VLR_TOTAL',
+            'QTD PARCELAS': 'QTD_PARCELAS',
+            'NUM PARCELA': 'NUM_PARCELA',
+        },
+        inplace=True,
+    )
+
+    # Exibindo as primeiras 5 linhas do DataFrame
+    print()  # Linha em branco para separar a saída de cada coluna
+    print(f'Amostra do arquivo com colunas renomeadas{df.head()}')
+
+    # Criando novas colunas
+    df['VLR_PARCELA'] = (df['VLR_TOTAL'] / df['QTD_PARCELAS']).round(2)
+
+    # Exibindo as primeiras 5 linhas do DataFrame
+    print()  # Linha em branco para separar a saída de cada coluna
+    print(f'Amostra do arquivo {df.head()}')
+
+    # Criando DataFrames separados
+    receita_despesa = (
+        df[['RECEITA_DESPESA']].drop_duplicates().reset_index(drop=True)
+    )
+    forma_pagto = df[['FORMA_PAGTO']].drop_duplicates().reset_index(drop=True)
+    tipo = df[['TIPO']].drop_duplicates().reset_index(drop=True)
+    categoria = df[['CATEGORIA']].drop_duplicates().reset_index(drop=True)
+    item = df[['ITEM']].drop_duplicates().reset_index(drop=True)
+    status = df[['STATUS']].drop_duplicates().reset_index(drop=True)
+
+    # Substituindo valores pelos índices
+    for col in [
+        'RECEITA_DESPESA',
+        'FORMA_PAGTO',
+        'TIPO',
+        'CATEGORIA',
+        'ITEM',
+        'STATUS',
+    ]:
+        df[col] = df[col].astype('category').cat.codes
+
+    # Criando DataFrame de despesas
+    df_despesas = df[
+        [
+            'DT_COMPRA',
+            'DESPESA',
+            'RECEITA_DESPESA',
+            'FORMA_PAGTO',
+            'TIPO',
+            'CATEGORIA',
+            'ITEM',
+            'VLR_TOTAL',
+            'QTD_PARCELAS',
+            'VLR_PARCELA',
+        ]
+    ]
+
+    # Exibindo as primeiras 5 linhas do DataFrame
+    print()  # Linha em branco para separar a saída de cada coluna
+    print(f'Amostra do arquivo {df_despesas.head()}')
+
+    # Criar um arquivo CSV a partir do DataFrame
+    df_despesas.to_csv(caminho_arquivo_saida, index=False)
+
+
+caminho_arquivo_entrada = './input/lanctos_receitas_despesas.csv'
+caminho_arquivo_saida = './output/despesas_receitas.csv'
+processar_csv(caminho_arquivo_entrada, caminho_arquivo_saida)
